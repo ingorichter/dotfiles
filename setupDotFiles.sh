b82d364383360c688b44b6aabe9683d8e6ed694d
Ingo Richter <ingorichter@IngosHackintosh.local>
2017-01-04 00:34:55 -0800

    - borrowed a theme for zsh - added zsh theme to link in home directory

--- setupDotFiles.sh
+++ setupDotFiles.sh
@@ -7,12 +7,42 @@ set -e
 # Link all dotfiles from $(pwd) into their respective location inside $HOME
 DOTFILES=$(pwd -P)
 
+# TODO:
+# - copy fonts into proper location
+# - adjust iterm to use that font
+
+# Use colors, but only if connected to a terminal, and that terminal
+# supports them.
+if which tput >/dev/null 2>&1; then
+	ncolors=$(tput colors)
+fi
+if [ -t 1 ] && [ -n "$ncolors" ] && [ "$ncolors" -ge 8 ]; then
+	RED="$(tput setaf 1)"
+	GREEN="$(tput setaf 2)"
+	YELLOW="$(tput setaf 3)"
+	BLUE="$(tput setaf 4)"
+	BOLD="$(tput bold)"
+	NORMAL="$(tput sgr0)"
+else
+	RED=""
+	GREEN=""
+	YELLOW=""
+	BLUE=""
+	BOLD=""
+	NORMAL=""
+fi
+
 printErrorMessage() {
 	printf "\e[0;31m  [✖] $1 $2\e[0m\n"
 }
 
+link_file() {
+	printf "Create link from ${GREEN}$2${NORMAL} to ${GREEN}$1${NORMAL}\n"
+	ln -s $1 $2
+}
+
 linkDotFiles() {
-	ln -s "${DOTFILES}/zsh/zshrc" "${HOME}/.zshrc"
+	link_file "${DOTFILES}/zsh/zshrc" "${HOME}/.zshrc"
 	# ln -s "${DOTFILES}/gitconfig" "${HOME}/.gitconfig"
 	# ln -s "${DOTFILES}/Gdbinit/gdbinit" "${HOME}/.gdbinit"
 	# ln -s "${DOTFILES}/ssh-keys-macpro" "${HOME}/.ssh"
@@ -23,10 +53,13 @@ linkDotFiles() {
     
     # Setup Fonts
     # cp -a "${HOME}/Dropbox/Fonts/" "${HOME}/Library/Fonts"
+
+	link_file "${DOTFILES}/zsh/themes/nick.zsh-theme" "${HOME}/.oh-my-zsh/themes/nick.zsh-theme"
 }
 
 unlinkDotFiles() {
 	rm "${HOME}/.zshrc"
+	rm "${HOME}/.oh-my-zsh/themes/nick.zsh-theme"
 
 	# setup all mvim related stuff
 	# rm "${HOME}/.vimrc"
