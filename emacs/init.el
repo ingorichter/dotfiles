5444d00f4ab3b6e49c6d5fa56e5fe01b89cab030
Ingo Richter <ingo.richter+github@gmail.com>
2020-08-18 14:35:44 +0200

    - enable server mode - disable Nextcloud sync for CalDAV - add some other plugins

--- emacs/init.el
+++ emacs/init.el
@@ -11,11 +11,14 @@
   (package-refresh-contents)
   (package-install 'use-package))
 
+(unless (and (fboundp 'server-running-p)
+  (server-running-p))
+  (server-start))
+
 (org-babel-load-file (expand-file-name "~/.emacs.d/Config.org"))
-(setenv "GPG_AGENT_INFO" nil)
+;;(setenv "GPG_AGENT_INFO" nil)
 
   ;; https://stackoverflow.com/questions/5052088/what-is-custom-set-variables-and-faces-in-my-emacs
 ;; (setq custom-file "~/.emacs.d/custom.el")
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
 (load custom-file)
-
