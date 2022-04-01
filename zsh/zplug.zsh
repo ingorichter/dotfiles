b161d7562ceaba2c69a67ba6870d1f1369c3ff24
Ingo Richter <ingo.richter+github@gmail.com>
2022-04-01 09:38:22 -0700

    add zplug.zsh and increase size of history file

---
+++ zsh/zplug.zsh
@@ -0,0 +1,19 @@
+source ${HOMEBREW_CELLAR}/zplug/2.4.2/init.zsh
+
+zplug "b4b4r07/zplug"
+zplug "Game4Move78/zsh-bitwarden"
+zplug "spaceship-prompt/spaceship-prompt", use:spaceship.zsh, from:github, as:theme
+zplug "wfxr/forgit"
+zplug "zsh-users/zsh-history-substring-search"
+zplug "zsh-users/zsh-syntax-highlighting"
+
+if ! zplug check --verbose; then
+  printf "Install? [y/N]: "
+  if read -q; then
+    echo; zplug install
+  fi
+fi
+
+zplug load --verbose
+
+[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
