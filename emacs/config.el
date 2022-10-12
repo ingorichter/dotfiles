307f3b15c2fdef962ac9e3b34cb5dd520cc1caaa
Ingo Richter <ingo.richter+github@gmail.com>
2022-10-12 14:26:20 +0200

    remove some duplicate modules and setup new theme

--- emacs/config.el
+++ emacs/config.el
@@ -1,23 +1,21 @@
 ;; Use crafted-package-install-package
 
-;;(require 'crafted-completion)
 (require 'crafted-defaults)
 (require 'crafted-updates)
+(require 'crafted-completion)
+(require 'crafted-ui)
 (require 'crafted-osx)
 (require 'crafted-editing)
 (require 'crafted-project)
 (require 'crafted-screencast)
-(require 'crafted-ui)
 (require 'crafted-package)
 (require 'crafted-windows)
 (require 'crafted-speedbar)
 (require 'crafted-org)
 (require 'crafted-compile)
-(require 'crafted-completion)
 
 ;; still my custom version
 (require 'crafted-git)
-;;(require 'crafted-code-completion)
 (require 'crafted-persistence)
 (require 'crafted-org)
 (require 'crafted-org-roam)
@@ -30,17 +28,10 @@
 ;; load my customizations and stuff that I want to use
 (load-file (expand-file-name "ingo-config.el" user-emacs-directory))
 
-;; (load-theme 'modus-operandi t)
-(crafted-package-install-package 'modus-themes)
-;; Add all your customizations prior to loading the themes
-(setq modus-themes-italic-constructs t
-      modus-themes-bold-constructs nil
-      modus-themes-region '(bg-only no-extend))
-
-;; Load the theme files before enabling a theme
-(modus-themes-load-themes)
-
-;; Load the theme of your choice:
-(modus-themes-load-operandi) ;; OR (modus-themes-load-vivendi)
-
-(define-key global-map (kbd "<f6>") #'modus-themes-toggle)
+(crafted-package-install-package 'ef-themes)
+(setq ef-themes-to-toggle '(ef-winter ef-frost))
+(setq ef-themes-mixed-fonts t
+      ef-themes-variable-pitch-ui t)
+;; Load the theme of choice:
+(load-theme 'ef-winter :no-confirm)
+(define-key global-map (kbd "<f6>") #'ef-themes-toggle)
