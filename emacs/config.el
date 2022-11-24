9452177d66f8241129ddee43fdb96a9943c18a1f
Ingo Richter <ingo.richter+github@gmail.com>
2022-11-23 16:50:23 -0800

    rename crafted-org.el to crafted-custom-org.el

--- emacs/config.el
+++ emacs/config.el
@@ -1,23 +1,25 @@
 ;; Use crafted-package-install-package
 
 (require 'crafted-defaults)
-(require 'crafted-updates)
+(require 'crafted-compile)
 (require 'crafted-completion)
-(require 'crafted-ui)
-(require 'crafted-osx)
 (require 'crafted-editing)
+(require 'crafted-ide)
+;; (require 'crafted-mastering-emacs)
+(require 'crafted-org)
+(require 'crafted-osx)
+(require 'crafted-package)
 (require 'crafted-project)
 (require 'crafted-screencast)
-(require 'crafted-package)
-(require 'crafted-windows)
 (require 'crafted-speedbar)
-(require 'crafted-org)
-(require 'crafted-compile)
+(require 'crafted-ui)
+(require 'crafted-updates)
+(require 'crafted-windows)
 
 ;; still my custom version
 (require 'crafted-git)
 (require 'crafted-persistence)
-(require 'crafted-org)
+(require 'crafted-org-ingo)
 (require 'crafted-org-roam)
 (require 'crafted-denote)
 
