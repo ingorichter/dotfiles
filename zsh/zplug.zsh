79a39c3bbba56c30f0a4a8f714bb72661cc47ba3
Ingo Richter <ingo.richter+github@gmail.com>
2023-06-20 15:04:56 -0700

    add lux plugin to zsh and fzf to tmux config

--- zsh/zplug.zsh
+++ zsh/zplug.zsh
@@ -11,6 +11,7 @@ zplug "romkatv/powerlevel10k", as:theme, depth:1
 zplug "zsh-users/zsh-syntax-highlighting", defer:2
 zplug "MichaelAquilina/zsh-you-should-use"
 zplug "DarrinTisdale/zsh-aliases-exa"
+zplug "pndurette/zsh-lux"
 
 if ! zplug check --verbose; then
   printf "Install? [y/N]: "
