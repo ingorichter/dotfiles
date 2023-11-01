e59a7712bcf73011c4a2585cec6f0e9e48bfcb80
Ingo Richter <ingo.richter+github@gmail.com>
2023-11-01 17:22:48 +0100

    update tmux config

--- zsh/fzf.zsh
+++ zsh/fzf.zsh
@@ -18,4 +18,4 @@ source "${FZF_SCRIPTS}/key-bindings.zsh"
 export FZF_DEFAULT_COMMAND='fd --type f --hidden --follow --exclude .git'
 
 # Previews
-export FZF_DEFAULT_OPTS='--preview "bat --color=always --style=numbers --line-range=:500 {}"'
+# export FZF_DEFAULT_OPTS='--preview "bat --color=always --style=numbers --line-range=:500 {}"'
