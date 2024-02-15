7812dcdcfac6aea47410e4d21e20d5d3330c08fd
Ingo Richter <ingo.richter+github@gmail.com>
2024-02-14 17:41:00 -0800

    add new zsh function to expose artifactory credentials

--- zsh/zplug.zsh
+++ zsh/zplug.zsh
@@ -1,19 +1,19 @@
 source ${HOMEBREW_CELLAR}/zplug/2.4.2/init.zsh
 
 zplug "b4b4r07/zplug"
-zplug "Game4Move78/zsh-bitwarden"
-zplug "mattberther/zsh-pyenv"
-zplug "wfxr/forgit"
-zplug "zsh-users/zsh-autosuggestions"
-zplug "zsh-users/zsh-history-substring-search"
-zplug "zsh-users/zsh-syntax-highlighting"
-zplug "romkatv/powerlevel10k", as:theme, depth:1
 zplug "zsh-users/zsh-syntax-highlighting", defer:2
-zplug "MichaelAquilina/zsh-you-should-use"
-zplug "pndurette/zsh-lux"
-zplug "favware/zsh-lerna"
+zplug "zsh-users/zsh-syntax-highlighting"
+zplug "zsh-users/zsh-history-substring-search"
+zplug "zsh-users/zsh-autosuggestions"
+zplug "wfxr/forgit"
 zplug "TwoPizza9621536/zsh-eza"
-zplug "marlonrichert/zsh-autocomplete"
+zplug "romkatv/powerlevel10k", as:theme, depth:1
+zplug "pndurette/zsh-lux"
+zplug "MichaelAquilina/zsh-you-should-use"
+zplug "mattberther/zsh-pyenv"
+zplug "marlonrichert/zsh-autocomplete", at:3942311
+zplug "Game4Move78/zsh-bitwarden"
+zplug "favware/zsh-lerna"
 
 if ! zplug check --verbose; then
   printf "Install? [y/N]: "
