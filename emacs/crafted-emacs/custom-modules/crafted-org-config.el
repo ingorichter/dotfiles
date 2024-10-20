1e9af5d2a1ba3e9f38f119796f73633b3aa60afc
Ingo Richter <ingo.richter+github@gmail.com>
2024-10-20 18:16:37 +0200

    update emacs config

--- emacs/crafted-emacs/custom-modules/crafted-org-config.el
+++ emacs/crafted-emacs/custom-modules/crafted-org-config.el
@@ -48,7 +48,32 @@
       '(("l" "A link, for reading later." entry
          (file+headline "notes-new.org" "Reading List")
          "* %:description\n%u\n\n%c\n\n%i"
-         :empty-lines 1)))
+         :empty-lines 1)
+        ("i" "Idea" entry (file+headline org-refile-location "Ideas")
+         "* Idea %^{Title} %^g\n  :LOGBOOK:\n - Added: %U\n:END:")))
+
+
+;; (setq org-capture-templates '(("t" "To Do Item" entry (file+headline org-refile-location "Inbox")
+;;                                "* TODO %^{Titel} %^g\n %?\n\n:LOGBOOK:\n - Added: %U\n:END:")
+;;                               ("a" "Appointment" entry (file org-gcal-location)
+;;                                "* %?\n\n%^T\n\n:PROPERTIES:\n\n:END:\n\n")
+;;                               ("n" "Note" entry (file+headline org-default-notes-file "Notes")
+;;                                "* Note %^{Titel} %^g\n  %?\n  :LOGBOOK:\n - Added: %U\n:END:")
+;;                               ("i" "Idee" entry (file+headline org-refile-location "Ideen")
+;;                                "* Idee %^{Titel} %^g\n  %?\n  :LOGBOOK:\n - Added: %U\n:END:")
+;;                               ("g" "Weekly Goals" entry
+;;                                (file+datetree org-weekly-goals (format-time-string "%Y"))
+;;                                "* %U\n\nHigh Level Ziele fuer die %(format-time-string "%W"). Woche\n - [ ] %(format-time-string "%W")$ x in die Spardose\n - [ ] Workout\n - [ ] Laufen")
+;;                               ("j" "Journal" entry (file+datetree org-journal-location)
+;;                                "* %U - %?\n  %i" :clock-in t :clock-resume t)
+;;                               ("s" "Team Status Update" entry (file+olp+datetree org-cicada-daily-status-location) "* Daily Status\n")
+;;                               ("d" "Review: Daily Review" entry
+;;                                (file+olp+datetree org-review-location)
+;;                                (file "~/Nextcloud/org/BASB/review/dailyreviewtemplate.org"))
+;;                               ("w" "Review: Weekly Review" entry
+;;                                (file+olp+datetree org-review-location)
+;;                                (file "~/Nextcloud/org/BASB/review/weeklyreviewtemplate.org"))))
+
 
 (org-reload)
 
