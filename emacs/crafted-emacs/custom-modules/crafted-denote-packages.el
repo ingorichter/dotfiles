3ea243356951ea8c189217dfea6b7358a6589c9b
Ingo Richter <ingo.richter+github@gmail.com>
2025-05-29 17:52:27 -0700

    enhance configuration files: added denote packages in Emacs, updated Kitty font settings and key mappings, introduced file picker plugin in Zellij, and implemented lazy loading for Zsh plugins with timing functions

--- emacs/crafted-emacs/custom-modules/crafted-denote-packages.el
+++ emacs/crafted-emacs/custom-modules/crafted-denote-packages.el
@@ -9,6 +9,10 @@
 
 (add-to-list 'package-selected-packages 'denote)
 (add-to-list 'package-selected-packages 'denote-journal)
+(add-to-list 'package-selected-packages 'denote-org)
+(add-to-list 'package-selected-packages 'denote-silo)
+(add-to-list 'package-selected-packages 'denote-markdown)
+(add-to-list 'package-selected-packages 'denote-sequence)
 (add-to-list 'package-selected-packages '(denote-menu :type git :repo "namilus/denote-menu"))
 
 (provide 'crafted-denote-packages)
