de7dad8344428a9c245067d302847f174f2bfc8a
Ingo Richter <ingo.richter+github@gmail.com>
2026-05-08 18:45:34 -0700

    emacs: add grove notes and harper-ls config
    - Add grove module for Obsidian-style org notes
    - Configure harper-ls grammar linter via eglot workspace settings
    - Add SomaFM Specials radio channel to empv

---
+++ emacs/crafted-emacs/custom-modules/crafted-grove-packages.el
@@ -0,0 +1,13 @@
+;;; crafted-grove-packages.el --- Obsidian style notes organizer  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2026  Ingo Richter
+
+;; Author: Ingo Richter <ingo.richter@gmail.com>
+;; Keywords: data
+
+;;; Code:
+
+(add-to-list 'package-selected-packages '(grove :type git :host github :repo "jonathanchu/grove"))
+
+(provide 'crafted-grove-packages)
+;;; crafted-grove-packages.el ends here
